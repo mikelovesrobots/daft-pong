2752f959a63fc5df5970612b9b60a4166e6a7acc
Mike Judge <mikelovesrobots@gmail.com>
2013-04-27 12:53:53 -0700

    Ball elasticity and speed tweaking

---
+++ app/Assets/Scripts/BallPaddleElasticity.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class BallPaddleElasticity : MonoBehaviour {
+    private const float FORCE = 40f;
+    private const float X_FORCE_DAMPENING = 0.1f;
+    private const string PADDLE_TAG = "Paddle";
+
+    void OnCollisionEnter(Collision collisionInfo) {
+        if (collisionInfo.gameObject.tag == PADDLE_TAG) {
+            var force = Force(collisionInfo.transform.position);
+            rigidbody.AddForce(force);
+        }
+    }
+
+    private Vector3 Force(Vector3 otherPosition) {
+        return Direction(otherPosition) * FORCE;
+    }
+
+    private Vector3 Direction(Vector3 otherPosition) {
+        var delta = transform.position - otherPosition;
+        delta.x *= X_FORCE_DAMPENING;
+        return delta.normalized;
+    }
+}
---
+++ app/Assets/Scripts/BallSpeedLimiter.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class BallSpeedLimiter : MonoBehaviour {
+    private const float SPEED_LIMIT = 10f;
+
+    void FixedUpdate() {
+        if (rigidbody.velocity.magnitude > SPEED_LIMIT) {
+            rigidbody.velocity = rigidbody.velocity.normalized * SPEED_LIMIT;
+            Debug.Log("Welcome to the dangerZONE");
+        }
+    }
+}
