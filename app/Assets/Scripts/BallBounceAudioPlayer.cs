a987f26eda86912f4b4b9c52a9fd2bac8b78a67e
Mike Judge <mikelovesrobots@gmail.com>
2013-04-28 12:02:18 -0700

    Debounce ball bouncing sound

--- app/Assets/Scripts/BallBounceAudioPlayer.cs
+++ app/Assets/Scripts/BallBounceAudioPlayer.cs
@@ -2,7 +2,19 @@ using UnityEngine;
 using System.Collections;
 
 public class BallBounceAudioPlayer : MonoBehaviour {
+    private const float WAIT_TIME = 0.02f;
+    private bool canPlay = true;
+
     void OnCollisionEnter() {
-        AudioPlayer.Instance.Bounce();
+        if (canPlay) {
+            AudioPlayer.Instance.Bounce();
+            canPlay = false;
+            StartCoroutine(WaitThenEnablePlaying());
+        }
+    }
+
+    private IEnumerator WaitThenEnablePlaying() {
+        yield return new WaitForSeconds(WAIT_TIME);
+        canPlay = true;
     }
 }
