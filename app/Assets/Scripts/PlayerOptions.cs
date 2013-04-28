82b882dbc4b3d7ba8e40055455b2478a46a99dd1
Mike Judge <mikelovesrobots@gmail.com>
2013-04-28 16:53:08 -0700

    Remove mikelovesrobots from PlayerOptions

--- app/Assets/Scripts/PlayerOptions.cs
+++ app/Assets/Scripts/PlayerOptions.cs
@@ -3,5 +3,5 @@ using System.Collections;
 
 public class PlayerOptions {
     public static Color Color = new Color(122f/255f, 196f/255f, 205f/255f, 9f/255f);
-    public static string Name = "mikelovesrobots";
+    public static string Name = "";
 }
